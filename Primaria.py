7663919f37c15244590e8a2ef407c1835c3b7a15
ThiagoIvens <thiagorech2121@gmail.com>
2021-03-21 22:36:26 -0300

    update Replicas v2

--- Primaria.py
+++ Primaria.py
@@ -3,7 +3,7 @@ import socket, threading
 HOST = '127.0.0.1'       # Endereco IP do Servidor
 PORT = 7000   # Porta que o Servidor esta
 USER_PORT = 6000
-listReplicas = [3000, 4000, 5000, 6000]
+listReplicas = [1000, 2000, 3000, 4000]
 
 class Saldo(object):
     def __init__(self, valor):
@@ -16,6 +16,17 @@ class Confirmado(object):
 saldo = Saldo(0)
 contador = Confirmado(0)
 
+def testarConn(conexao, port): 
+    try:
+        return conexao.connect((HOST, port))
+    except:
+        testarConn(conexao, PORT)
+
+def enviaMsg(port, data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conexao:
+        testarConn(conexao, port)
+        conexao.sendall(data.encode('utf-8'))
+
 def main():
     t = threading.Thread(target = threadOfReceived) # Define a função threadOfReceived como thread
     t.start() # Inicia a Thread acima
@@ -27,50 +38,46 @@ def threadOfReceived(): # função para ficar a espera da mensagem do sevidor
     s.listen() # começa a escutar no destino definido
     while True:
         con, port = s.accept() # define a conexao e atribui a variavel conn, quando o servidor aceitar
-        
-        msg = con.recv(1024) # pega os dados recebidos e atribui a variavel msg
-        if not msg: break # se msg estiver vazio para todo o processo
-        data = msg.decode().split('|') # decodifica a mensagem de bytes para string e separa por |, transformando em uma lista de string
-        id = data[0] # atribui o primeiro valor da lista a variavel id
-        operation = data[1] # atribui o segundo valor da lista a variavel operation
-        valor = int(data[2]) # atribui o terceiro valor da lista a variavel valor
-        print(data)
-    
-        if operation == 'CREDITO':
-            saldo._saldo += valor
-            print('Cliente requisitou Crédito\nNovo saldo é de', saldo._saldo, ', enviando para comparação...')
-            msg = str(id)+'|COMPARE|CREDITO|'+str(valor)
-            for replica in listReplicas:
-                enviaMsg(replica, msg)
+        try:
+            while True:
+                msg = con.recv(1024) # pega os dados recebidos e atribui a variavel msg
+                if not msg: break # se msg estiver vazio para todo o processo
+                data = msg.decode().split('|') # decodifica a mensagem de bytes para string e separa por |, transformando em uma lista de string
+                id = data[0] # atribui o primeiro valor da lista a variavel id
+                operation = data[1] # atribui o segundo valor da lista a variavel operation
+                valor = int(data[2]) # atribui o terceiro valor da lista a variavel valor
+                print(data)
 
-        elif (operation == "DEBITO"):
-            saldo._saldo -= valor
-            print('Cliente requisitou Débito\nNovo saldo é de', saldo._saldo, ', enviando para comparação...')
-            msg = str(id)+'|COMPARE|DEBITO|'+str(valor)
-            for replica in listReplicas:
-                enviaMsg(replica, msg)
+                if operation == 'CREDITO':
+                    saldo._saldo += valor
+                    print('Cliente requisitou Crédito\nNovo saldo experado é', saldo._saldo, ', enviando para comparação...')
+                    msg = str(id)+'|COMPARE|CREDITO|'+str(valor)
+                    for replica in listReplicas:
+                        enviaMsg(replica, msg)
 
-        elif (operation == "OK"):
-            # faz o codigo de OK
-            contador._confirmado += 1
-            if contador._confirmado == 4:
-                print('Todas replicas retornaram OK')
-                enviaMsg(USER_PORT, msg)
-                contador_confirmados = 0    
-        
-            print('fechou conexao')
-            s.close() # fexa a conexao com o servidor
-        elif (operation == "ERRO"):
-            print('ERRO')
-            msg = str(id)+'|ERRO|'+str(saldo._saldo)
-            enviaMsg(USER_PORT, msg)
+                elif (operation == "DEBITO"):
+                    saldo._saldo -= valor
+                    print('Cliente requisitou Débito\nNovo saldo é de', saldo._saldo, ', enviando para comparação...')
+                    msg = str(id)+'|COMPARE|DEBITO|'+str(valor)
+                    for replica in listReplicas:
+                        enviaMsg(replica, msg)
+
+                elif (operation == "OK"):
+                    # faz o codigo de OK
+                    contador._confirmado += 1
+                    if contador._confirmado == 4:
+                        print('Todas replicas retornaram OK')
+                        enviaMsg(USER_PORT, msg)
+                        contador._confirmado = 0    
                 
-
-def enviaMsg(port, data):
-    conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conexao.connect((HOST, port))
-    conexao.sendall(data.encode('utf-8'))
-
+                    print('fechou conexao')
+                    s.close() # fexa a conexao com o servidor
+                elif (operation == "ERRO"):
+                    print('ERRO')
+                    msg = str(id)+'|ERRO|'+str(saldo._saldo)
+                    enviaMsg(USER_PORT, msg)
+        finally:
+            s.close()
 
 if __name__ == "__main__":
     main()
--- Replica2.py
+++ Replica2.py
@@ -16,9 +16,38 @@ class Contador(object):
 saldo = Saldo(0)
 contador = Contador(0)
 
-def main():
-    t = threading.Thread(target = threadOfReceived) # Define a função threadOfReceived como thread
-    t.start() # Inicia a Thread acima
+def comparar():
+    if(len(listValues) == 5):
+        for i in len(listValues):
+            if(listValues[0] != listValues[i]):
+                return False
+        return True
+        
+def testarConn(conexao, port): 
+    try:
+        return conexao.connect((HOST, port))
+    except:
+        testarConn(conexao, PORT)
+
+def enviaMsg(port, data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conexao:
+        testarConn(conexao, port)
+        conexao.sendall(data.encode('utf-8'))
+        print("Mensagem enviada para a Réplica de porta: ", port)
+
+def codeDentrodoIF(msg, valor, operation):
+    print('entrou no code dentro do if')
+    msg = msg.decode('utf-8')
+    contador.contador +=1
+    if operation == "CREDITO":
+        saldo._saldo += valor
+    elif operation == "DEBITO":
+        saldo._saldo -= valor
+    saldoAnterior = saldo._saldo
+    listValues.append(saldo._saldo)
+    for replica in listReplicas:
+        enviaMsg(replica, msg)
+        saldo._saldo = saldoAnterior
 
 def threadOfReceived(): # função para ficar a espera da mensagem do sevidor
     saldo._saldo
@@ -29,62 +58,48 @@ def threadOfReceived(): # função para ficar a espera da mensagem do sevidor
     s.listen() # começa a escutar no destino definido
     while True:
         con, port = s.accept() # define a conexao e atribui a variavel conn, quando o servidor aceitar
-        msg = con.recv(1024)
-        if not msg: break
-        data = msg.decode().split('|')
-        id = data[0] # atribui o primeiro valor a variavel id
-        operation1 = data[1] # atribui o segundo valor a variavel operation
-        operation2 = data[2]
-        valor = int(data[3]) # atribui o terceiro valor a variavel valor
-        
-        print(data)
+        try:
+            while True:
+                msg = con.recv(1024)
+                if not msg: break
+                data = msg.decode().split('|')
+                id = data[0] # atribui o primeiro valor a variavel id
+                operation1 = data[1] # atribui o segundo valor a variavel operation
+                operation2 = data[2]
+                valor = int(data[3]) # atribui o terceiro valor a variavel valor
+                
+                print(data)
 
-        def comparar():
-                if(len(listValues) == 5):
-                    for i in len(listValues):
-                        if(listValues[0] != listValues[i]):
-                            return False
-                    return True
-
-        def enviaMsg(port, data):
-            conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            conexao.connect((HOST, port))
-            conexao.sendall(data)
-
-        def codeDentrodoIF(msg, valor, operation):
-            contador.contador +=1
-            if operation == "CREDITO":
-                saldo._saldo += valor
-            elif operation == "DEBITO":
-                saldo._saldo -= valor
-            saldoAnterior = saldo._saldo
-            listValues.append(saldo._saldo)
-            for replica in listReplicas:
-                enviaMsg(replica, msg)
-                saldo._saldo = saldoAnterior
-
-        if (operation1 == "COMPARE"):
-            # faz o codigo de OK
-            codeDentrodoIF(msg, valor, operation2)
-            
-            if contador.contador == 5 or len(listValues) == 5:
-                if comparar:
-                    print('Todas replicas retornaram OK')
-                    msg = str(id)+'|OK|'+str(saldo._saldo)
-                    enviaMsg(SERVER_PORT, msg)
-                    contador.contador = 0
-                else:
-                    print('Todas replicas retornaram OK')
-                    msg = str(id)+'|ERRO|'+str(saldo._saldo)
-                    enviaMsg(SERVER_PORT, msg)
-                    contador.contador = 0
-            
-            elif len(listValues) != 5 and contador.contador !=5:
-                for replica in listReplicas:
-                    enviaMsg(replica, msg)
+                if (operation1 == "COMPARE"):
+                    print('entrou no if')
+                    # faz o codigo de OK
+                    codeDentrodoIF(msg, valor, operation2)
                     
-            print('fechou conexao')
-            s.close() # fexa a conexao com o servidor
+                    if contador.contador == 5 or len(listValues) == 5:
+                        if comparar:
+                            print('Todas replicas retornaram OK!')
+                            msg = str(id)+'|OK|'+str(saldo._saldo)
+                            enviaMsg(SERVER_PORT, msg)
+                            contador.contador = 0
+                        else:
+                            print('Deu ERRO no calculo!')
+                            msg = str(id)+'|ERRO|'+str(saldo._saldo)
+                            enviaMsg(SERVER_PORT, msg)
+                            contador.contador = 0
+                    
+                    elif len(listValues) != 5 or contador.contador !=5:
+                        contadorLista = 0
+                        if(contadorLista == 3):
+                            for replica in listReplicas:
+                                msg = msg.decode('utf-8')
+                                enviaMsg(replica, msg)
+                                contadorLista += 1
+        finally:
+            s.close()
+
+def main():
+    t = threading.Thread(target = threadOfReceived) # Define a função threadOfReceived como thread
+    t.start() # Inicia a Thread acima
 
 if __name__ == "__main__":
     main()
